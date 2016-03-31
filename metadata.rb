b6a206db9c8d74c7834ef7ffb068cd8c588f7ed5
Yomi Ogunyinka <Yomi.O@JaghoConsultant.co.uk>
2016-03-31 16:10:26 +0100

    Metadata updated.

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'git-jscookbook'
+maintainer       'J S Sainsbury PLC'
+maintainer_email 'Yomi.O@JaghoConsultants.co.uk'
+license          'All rights reserved'
+description      'Installs/Configures git-jscookbook'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
